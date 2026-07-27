84ca43acf2791153d1cf40a409afaf0f583882fd
samkazemii <s5060681422@gmail.com>
2026-07-27 22:33:02 +0300

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -2,5 +2,5 @@
 // Never place a service_role or secret key in this file.
 window.SK_SUPABASE = {
   url: 'https://kmckpbtowbfqgstbegck.supabase.co',
-  key: 'sb_publishable_vg41SXH7gKlq-GumPsuhNA_2yEd3ecS'
+  key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImttY2twYnRvd2JmcWdzdGJlZ2NrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUxNzE2ODUsImV4cCI6MjEwMDc0NzY4NX0.v-lAAhWBwKEFCReKo0Lxxsz9fWTSDKQxC7rCMrCrNo8'
 };
